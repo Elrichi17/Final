9595c69150b937cf0045b86a03675586c01e61fa
ElRicky17 <david.ricaurte@eia.edu.co>
2024-11-17 00:40:57 -0500

    ya casi

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,17 +14,17 @@ import { PaymentsModule } from './Payments/payments.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    
+   TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  autoLoadEntities: true,
+  synchronize: true,
+}),
+
     TypeOrmModule.forFeature([Event, Membership, Model, Photo, Product]),
     ModelsModule,ProductsModule,PhotosModule,MembershipsModule,EventsModule,PaymentsModule
   ],
