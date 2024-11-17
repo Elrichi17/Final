9831677f819df86234062f3667301cb7fc65e8af
ElRicky17 <david.ricaurte@eia.edu.co>
2024-11-17 01:10:52 -0500

    ya casi muero

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -17,10 +17,14 @@ import { PaymentsModule } from './Payments/payments.module';
    TypeOrmModule.forRoot({
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
+/*   port: parseInt(process.env.DB_PORT, 10),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME, */
+  url: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Esto desactiva la verificación estricta del certificado, útil si Render no provee un certificado específico
+  },
   autoLoadEntities: true,
   synchronize: true,
 }),
